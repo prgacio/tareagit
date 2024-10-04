a90e2be9ba0213be4a7a3c0dd39f94b9fe9369b2
Patricia <prgacio@gmail.com>
2024-10-04 17:48:53 +0200

    primer commit con index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        $num1 = rand(1,100);
+        $num2 = rand(1, 100);
+
+        $mult = $num1 * $num2;
+
+        echo "<p>Número aleatorio 1: $num1</p>";
+        echo "<p>Número aleatorio 2: $num1</p>";
+
+    ?>
+    
+</body>
+</html>
